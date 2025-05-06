b560d89791db85b9c23ae51829beac0d0712e359
tom <tommckeown909@gmail.com>
2025-05-06 12:13:15 +0100

    Improve Playwright suite

--- e2e/playwright.config.ts
+++ e2e/playwright.config.ts
@@ -8,6 +8,11 @@ import dotenv from "dotenv";
 import path from "path";
 dotenv.config({ path: path.resolve(__dirname, ".env") });
 
+const baseURL = process.env.BASE_URL;
+if (!baseURL) {
+	throw new Error("BASE_URL environment variable must be set");
+}
+
 /**
  * See https://playwright.dev/docs/test-configuration.
  */
@@ -26,7 +31,7 @@ export default defineConfig({
 	/* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
 	use: {
 		/* Base URL to use in actions like `await page.goto('/')`. */
-		baseURL: "https://localhost:3000",
+		baseURL,
 
 		/* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
 		trace: "on-first-retry",
--- e2e/tests/first-test.spec.ts
+++ e2e/tests/first-test.spec.ts
@@ -1,17 +1,76 @@
-import { test } from "@playwright/test";
+import { test, expect, Page } from "@playwright/test";
 
-const username = process.env.USER1 ?? "";
-const password = process.env.PASSWORD1 ?? "";
+const username = process.env.USER1;
+const password = process.env.PASSWORD1;
+
+if (!username || !password) {
+	throw new Error("Please set USER1 and PASSWORD1 environment variables");
+}
+
+class LoginPage {
+	// Constructor
+	constructor(private page: Page) {}
+
+	// Locators
+	readonly loginButton = "data-testid=login-button";
+	readonly usernameInput = "data-testid=username-input";
+	readonly passwordInput = "data-testid=password-input";
+	readonly loginSubmitButton = "data-testid=login-submit-button";
+	readonly loginTitle = "data-testid=login-title";
+
+	// Methods
+	async login(username: string, password: string) {
+		await this.page.locator(this.loginButton).click();
+		await this.page.waitForSelector(this.loginTitle);
+		await this.page.locator(this.usernameInput).fill(username);
+		await this.page.locator(this.passwordInput).fill(password);
+		await this.page.locator(this.loginSubmitButton).click();
+	}
+}
+
+class MoviePage {
+	// Cosntructor
+	constructor(private page: Page) {}
+
+	// Locators
+	readonly movieLink = "data-testid=movie-link-0";
+
+	// Methods
+	async movieHasTitle(title: RegExp) {
+		await this.page.waitForSelector(this.movieLink);
+		await expect(this.page.locator(this.movieLink)).toHaveText(title);
+	}
+
+	async clickMovieLink() {
+		await this.page.waitForSelector(this.movieLink);
+		await this.page.locator(this.movieLink).click();
+	}
+}
+
+test.beforeEach(async ({ page }) => {
+	await page.goto("");
+});
 
 test("has title", async ({ page }) => {
-	await page.goto("");
-	await page.locator("data-testid=login-button").click();
-	await page.waitForSelector("data-testid=login-title");
-	await page.locator("data-testid=username-input").fill(username);
-	await page.locator("data-testid=password-input").fill(password);
-	await page.locator("data-testid=login-submit-button").click();
-	await page.waitForSelector("data-testid=movie-link-0");
-	await page.locator("data-testid=movie-link-0").click();
+	// await page.goto("");
+	const loginPage = new LoginPage(page);
+	await loginPage.login(username, password);
+
+	// await page.locator("data-testid=login-button").click();
+	// await page.waitForSelector("data-testid=login-title");
+	// await page.locator("data-testid=username-input").fill(username);
+	// await page.locator("data-testid=password-input").fill(password);
+	// await page.locator("data-testid=login-submit-button").click();
+
+	const moviePage = new MoviePage(page);
+	await moviePage.movieHasTitle(/.*Dune.*/);
+	await moviePage.clickMovieLink();
+
+	// await page.waitForSelector("data-testid=movie-link-0");
+	// await expect(page.locator("data-testid=movie-link-0")).toHaveText(
+	// 	/.*Dune.*/
+	// );
+	// await page.locator("data-testid=movie-link-0").click();
 
 	await page.waitForTimeout(5000);
 	// Expect a title "to contain" a substring.
